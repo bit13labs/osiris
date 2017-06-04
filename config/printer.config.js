58bbf8ce17f1e6cd0efeb2a665191e4389402e46
camalot <camalot@gmail.com>
2017-06-03 23:07:25 -0500

    fucking /g flag in regex

--- config/printer.config.js
+++ config/printer.config.js
@@ -8,13 +8,15 @@ module.exports = {
 		MACHINE_TYPE: "RepRap",
 		EXTRUDER_COUNT: 0,
 		UUID: '00000000-0000-0000-0000-000000000000',
-		EEPROM: null,
-		AUTOREPORT_TEMP: null,
-		PROGRESS: null,
-		AUTOLEVEL: null,
-		Z_PROBE: null,
-		SOFTWARE_POWER: null,
-		TOGGLE_LIGHTS: null,
-		EMERGENCY_PARSER: null
+		capabilities: {
+			EEPROM: null,
+			AUTOREPORT_TEMP: null,
+			PROGRESS: null,
+			AUTOLEVEL: null,
+			Z_PROBE: null,
+			SOFTWARE_POWER: null,
+			TOGGLE_LIGHTS: null,
+			EMERGENCY_PARSER: null
+		}
 	}
 };
--- config/regex.config.js
+++ config/regex.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
 	regex: {
-		"M105": /^\s*ok\sT:(\d{1,})\s\/(\d{1,})\sB:(\d{1,})\s\/(\d{1,})\s@:(\d{1,})\sB@:(\d{1,})/gi,
+		"M105": /^T:(\d{1,})\s\/(\d{1,})\sB:(\d{1,})\s\/(\d{1,})\s@:(\d{1,})\sB@:(\d{1,})/i,
 		// just to check it
-		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/g,
-		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):([0-1])$/g
+		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/,
+		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):(\d)$/
 	}
 }
