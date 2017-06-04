f7bc4fb0413b3c818c8d09aa030ec7cf0d78f988
camalot <camalot@gmail.com>
2017-06-03 21:30:17 -0500

    wip - work on parsing capabilities

---
+++ config/printer.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+	printer: {
+		FIRMWARE_NAME: "Unknown",
+		SOURCE_CODE_URL: null,
+		PROTOCOL_VERSION: 1.0,
+		MACHINE_TYPE: "RepRap",
+		EXTRUDER_COUNT: 0,
+		UUID: '00000000-0000-0000-0000-000000000000',
+		EEPROM: null,
+		AUTOREPORT_TEMP: null,
+		PROGRESS: null,
+		AUTOLEVEL: null,
+		Z_PROBE: null,
+		SOFTWARE_POWER: null,
+		TOGGLE_LIGHTS: null,
+		EMERGENCY_PARSER: null
+	}
+};
--- config/regex.config.js
+++ config/regex.config.js
@@ -3,5 +3,8 @@
 module.exports = {
 	regex: {
 		"M105": /^\s*ok\sT:(\d{1,})\s\/(\d{1,})\sB:(\d{1,})\s\/(\d{1,})\s@:(\d{1,})\sB@:(\d{1,})/gi,
+		// just to check it
+		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/g,
+		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):([0-1])$/g
 	}
 }
