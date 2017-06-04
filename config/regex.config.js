cbf3a235d795b0fd8341d58a82da4363eb553bc7
camalot <camalot@gmail.com>
2017-06-04 01:59:21 -0500

    return promise

--- config/regex.config.js
+++ config/regex.config.js
@@ -6,6 +6,9 @@ module.exports = {
 		"M105": /^(?:ok\s+)?T:(\d{1,})\s\/(\d{1,})\sB:(\d{1,})\s\/(\d{1,})\s@:(\d{1,})\sB@:(\d{1,})/g,
 		// just to check it
 		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/,
-		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):(\d)$/
+		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):(\d)$/,
+		"M31": /^(?:echo:\s*)Print time:(.*$)/,
+
+		"POSITION": /^X:(\d{1,}\.\d{2})\sY:(\d{1,}\.\d{2})\sZ:(\d{1,}\.\d{2})\sE:(\d{1,}\.\d{2})\s*/
 	}
 }
