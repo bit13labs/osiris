e4b9bb07fe762405a301b982f138a5b3a970f2f5
camalot <camalot@gmail.com>
2017-06-05 22:15:24 -0500

    promise some things. - handle error output - handle echo commands - trigger the events for the echo'd values

--- config/regex.config.js
+++ config/regex.config.js
@@ -9,12 +9,15 @@ module.exports = {
 		// just to check it
 		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/,
 		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):(\d)$/,
-		"M360": /^Config:([A-Za-z_\.0-9]+):(.*)$/m,
+		// repetier
+		//"M360": /^Config:([A-Za-z_\.0-9]+):(.*)$/m,
 		"M31": /^(?:echo:\s*)Print time:(.*$)/,
 		"M111": /^(?:echo:\s*)DEBUG:(?:(off|ECHO|INFO|ERRORS|DRYRUN|COMMUNICATION),?)+$/,
 		"M111_TEST": /(?:(off|ECHO|INFO|ERRORS|DRYRUN|COMMUNICATION),?)/g,
 		"M114": /^X:(\d{1,}\.\d{2})\sY:(\d{1,}\.\d{2})\sZ:(\d{1,}\.\d{2})\sE:(\d{1,}\.\d{2})\s*/,
 
-		"BUSY": /^(?:echo:)busy:\s?(.*)$/
+		"ECHO": /^(?:echo:?)?\s*([GM]\d{1,3})\s(.*$)/i,
+		"BUSY": /^(?:echo:)busy:\s?(.*)$/,
+		"ERROR": /^(?:echo:)?(?:Error:)(.*$)/i
 	}
 }
