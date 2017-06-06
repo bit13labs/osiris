82b81011038129c4c1bf5f8a68e279a76d6b92f9
camalot <camalot@gmail.com>
2017-06-05 21:47:18 -0500

    added M160 parse support

--- config/regex.config.js
+++ config/regex.config.js
@@ -3,14 +3,16 @@
 module.exports = {
 	regex: {
 		// this has an optional 'ok', because M105 puts ok at the start, M155 does not
-		"M105": /^(?:ok\s+)?(?:T(\d?):(\d{1,}(?:\.\d{1,})?)\s\/(\d{1,}(?:\.\d{1,})?)\s)+B:(\d{1,}(?:\.\d{1,})?)\s\/(\d{1,}(?:\.\d{1,})?)(?:\s(\d?)@:(\d{1,}))+\sB@:(\d{1,})/,
+		"M105": /^(?:ok\s+)?(?:T(\d?):(\d{1,}(?:\.\d{1,})?)\s\/(\d{1,}(?:\.\d{1,})?)\s)+(?:B:(\d{1,}(?:\.\d{1,})?)\s\/(\d{1,}(?:\.\d{1,})?))?(?:\s?(\d?)@:(\d{1,}))+(?:\sB@:(\d{1,}))?$/,
 		"M105_TOOLS": /(?:(T|B)(\d?):(\d{1,}(?:\.\d{1,})?)\s\/(\d{1,}(?:\.\d{1,})?)\s)/g,
 		"M105_TOOLS_OFFSET": /\s(?:(\d?|B)@:(\d{1,}))/g,
 		// just to check it
 		"M115_FIRMWARE": /^\s?(FIRMWARE_NAME):/,
 		"M115_CAPABILITY": /^Cap:([A-Z_0-9]+):(\d)$/,
+		"M360": /^Config:([A-Za-z_\.0-9]+):(.*)$/m,
 		"M31": /^(?:echo:\s*)Print time:(.*$)/,
-
+		"M111": /^(?:echo:\s*)DEBUG:(?:(off|ECHO|INFO|ERRORS|DRYRUN|COMMUNICATION),?)+$/,
+		"M111_TEST": /(?:(off|ECHO|INFO|ERRORS|DRYRUN|COMMUNICATION),?)/g,
 		"M114": /^X:(\d{1,}\.\d{2})\sY:(\d{1,}\.\d{2})\sZ:(\d{1,}\.\d{2})\sE:(\d{1,}\.\d{2})\s*/,
 
 		"BUSY": /^(?:echo:)busy:\s?(.*)$/
