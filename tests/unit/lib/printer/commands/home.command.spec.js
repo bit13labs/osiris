a055abff673fa70a9b469a17a2cf06cf4e16c527
Malik Muratovic <malik.muratovic@imanage.com>
2017-06-09 14:39:19 -0500

    moar tests

--- tests/unit/lib/printer/commands/home.command.spec.js
+++ tests/unit/lib/printer/commands/home.command.spec.js
@@ -19,6 +19,110 @@ describe("/lib/printer/commands/home.command.js => new()", () => {
       expect(cmd.printer).not.toBe(null || undefined);
       done();
     });
+    it("must execute without parameters", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      cmd.execute().then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28');
+      });
+      done();
+    });
+    it("must execute for X axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [true, false, false];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 X');
+      });
+      done();
+    });
+    it("must execute for Y axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [false, true, false];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 Y');
+      });
+      done();
+    });
+    it("must execute for Z axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [false, false, true];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 Z');
+      });
+      done();
+    });
+    it("must execute for X and Y axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [true, true, false];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 X Y');
+      });
+      done();
+    });
+    it("must execute for X and Z axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [true, false, true];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 X Z');
+      });
+      done();
+    });
+    it("must execute for Y and Z axis only", (done) => {
+      config.app.connected = false;
+      let cmd = new HomeCommand();
+      expect(cmd).not.toBe(null || undefined);
+      expect(cmd.printer).not.toBe(null || undefined);
+      let [x, y, z] = [false, true, true];
+      cmd.execute(x, y, z).then( (response) => {
+        expect(response).not.toBe(null || undefined);
+        expect(response.message).not.toBe(null || undefined);
+        expect(response.gcode).not.toBe(null || undefined);
+        expect(response.message).toBe('ok');
+        expect(response.gcode).toBe('G28 Y Z');
+      });
+      done();
+    });
   });
 });
 /* jshint ignore:end */
